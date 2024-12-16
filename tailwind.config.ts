57eee40ba6f3b60a956a679c44a6c19cb04f050b
Your Name <you@example.com>
2024-12-16 16:17:06 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        text:"#22202E",
+        
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
